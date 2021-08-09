c947851af695f7c159dd166ca6a323c061cb46b2
Nitya3008 <84838358+Nitya3008@users.noreply.github.com>
2021-08-09 17:03:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+
+var ball;
+var blower;
+var blowerMouth;
+
+
+
+function setup() {
+  createCanvas(800,400);
+
+ 
+  
+
+ ball=new Ball(250,200,80,80);
+ blower=new Blower(350,230,90,100);
+ blowerMouth=new Blowermouth(300,230,100,150);
+
+ button=createButton("Click to Blow");
+ button.position(width/2,height-100);
+ button.class(blowButton);
+ button.mousePressed(blow);
+
+}
+
+function draw() {
+  background(255,255,255);  
+
+
+  ball.show();
+  blower.show();
+  blowerMouth.show();
+  drawSprites();
+}
+
+function blow(){
+  Matter.Body.applyForce(ball.body,{x:0,y:0},{x:0,y:0.05});
+}
